a4b4d614e551c8df10979cabd16ad93fd3348ce6
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-02-09 21:12:56 -0300

    services

--- src/shared/pages/Service/index.tsx
+++ src/shared/pages/Service/index.tsx
@@ -1,3 +1,18 @@
+import * as C from './style';
+
 export const Service = () => {
-  return <div>index</div>;
+  return (
+    <C.Section>
+      <C.Title>Serviços</C.Title>
+      <C.Row direction="none">
+        <C.Item>Automações</C.Item>
+      </C.Row>
+      <C.Row direction="reverse">
+        <C.Item>Implantação de e-commerce</C.Item>
+      </C.Row>
+      <C.Row direction="none">
+        <C.Item>Otimização de sites (SEO)</C.Item>
+      </C.Row>
+    </C.Section>
+  );
 };
---
+++ src/shared/pages/Service/style.ts
@@ -0,0 +1,55 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* background: lightblue; */
+`;
+
+export const Title = styled.h1`
+  font-family: 'Monument Bold', sans-serif;
+  text-transform: uppercase;
+  font-size: 4rem;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+`;
+
+const move = keyframes`
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+`;
+
+interface IRowProps {
+  direction: 'none' | 'reverse';
+}
+
+export const Row = styled.div`
+  /* background: #fff; */
+  white-space: nowrap;
+  margin: 1rem 0;
+  display: flex;
+
+  animation: ${move} 20s linear infinite
+    ${({ direction }: IRowProps) => direction};
+`;
+
+export const Item = styled.div`
+  font-family: 'Monument Regular', sans-serif;
+  text-transform: uppercase;
+  font-size: 2rem;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
